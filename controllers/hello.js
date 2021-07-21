e6f59031961170fd3f11dfd754ca5d9fa6d58158
Mikhail Konevshchinskiy <mikhail.konevshchinskiy@dataart.com>
2021-07-21 18:18:13 +0300

    fix: Adding query validation

--- controllers/hello.js
+++ controllers/hello.js
@@ -4,6 +4,10 @@ const logger = log4js.getLogger('greetings');
 logger.level = 'info';
 
 exports.getGreetingsMessage = async (req, res, next) => {
+  if (!req.query?.name?.trim()) {
+    return res.status(400).json({ message: 'Query has not to be empty' });
+  }
+
   const { name } = req.query;
 
   try {
